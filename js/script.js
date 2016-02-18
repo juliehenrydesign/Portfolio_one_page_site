ba93b41639bf3d74f215144fc70c31ed6671fc93
Julie Henry <julie@juliehenrydesignllc.com>
2016-02-18 18:56:58 -0500

    added one pg site, first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$('nav').onePageNav({
+		scrollThreshold: 0.4,
+		navItems: "a:not(.submenu a)"
+	});
+
+function toggleNav() {
+	$('nav ul').slideToggle();
+}
+			
+$('#menu-button').click(toggleNav);
+
+function windowResize() {
+	var width = $(window).width();
+	var menu = $('nav ul');
+
+	if(width > 768 && menu.is(':hidden')){
+		menu.removeAttr('style');
+	}
+}
+
+$(window).resize(windowResize);
+			
+// function toggleChildren(event) {
+// 	event.preventDefault();
+// 	$('nav ul ul li').slideToggle();
+// }
+});
